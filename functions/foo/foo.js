b30c25163b35469a56d054af45eb95b30ce2edca
Jan Piotrowski <piotrowski+github@gmail.com>
2020-09-16 23:41:39 +0200

    Initial attempt

---
+++ functions/foo/foo.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const path = require("path")
+const { PrismaClient } = require('@prisma/client')
+
+exports.handler = async (event, context) => {
+
+  console.log("process.env.LAMBDA_TASK_ROOT", process.env.LAMBDA_TASK_ROOT)
+  fs.readdir(process.env.LAMBDA_TASK_ROOT + "", function(err, items) {
+    console.log("# content of process.env.LAMBDA_TASK_ROOT")
+    console.log(items);
+  })
+  fs.readdir(process.env.LAMBDA_TASK_ROOT + "/src", function(err, items) {
+    console.log("# content of process.env.LAMBDA_TASK_ROOT/src")
+    console.log(items);
+  })
+  fs.readdir(process.env.LAMBDA_TASK_ROOT + "/src/functions", function(err, items) {
+    console.log("# content of process.env.LAMBDA_TASK_ROOT/src/functions")
+    console.log(items);
+  })
+  fs.readdir(process.env.LAMBDA_TASK_ROOT + "/src/functions/image", function(err, items) {
+    console.log("# content of process.env.LAMBDA_TASK_ROOT/src/functions/image")
+    console.log(items);
+  })
+ 
+
+  var imagepath = path.resolve(process.env.LAMBDA_TASK_ROOT, 'src/functions/image', 'netlify.png')
+
+  const content = fs.readFileSync(imagepath)
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'image/png',
+    },
+    body: content.toString('base64'),
+    isBase64Encoded: true
+  };
+};
